6b5ec0f92482bc2f30e6a8245546f537171fd4b7
terenzio <terenzio@gmail.com>
2013-07-26 10:16:26 +0800

    push 1

--- Advanced/PushNotifications/src/com/example/parsepush/MainActivity.java
+++ Advanced/PushNotifications/src/com/example/parsepush/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.parsepush;
 
 import android.os.Bundle;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
 import android.app.Activity;
 import android.view.Menu;
 
@@ -10,7 +14,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		  Parse.initialize(this, "BEfHGc1EGoiA6ZKLhLOPS5fdiXlbVBSjc7R12B8u", "Xr9rDHfjwabLi3mJ22F28YsAjtYgS0uZMWII7cPx");
+		  
+		  PushService.setDefaultPushCallback(this, MainActivity.class);
+		  ParseInstallation.getCurrentInstallation().saveInBackground();
+		  
+		  ParseAnalytics.trackAppOpened(getIntent());
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
